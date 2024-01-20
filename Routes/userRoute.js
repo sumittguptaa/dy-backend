f6d45f89f32f1e9eada6f1c51550174188e11c0e
sumitbonatra <sumit@bonatra.com>
2024-01-20 11:28:59 +0530

    resolved comments

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const userController = require('../Controllers/userController');
-const authMiddleware = require('../middlewares/authMiddleware');
+const express = require("express");
+const userController = require("../Controllers/userController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-
-router.post('/signup', userController.signup);
-router.post('/signin', userController.signin); 
-router.post('/forgot-password', userController.forgotPassword); 
-router.post('/reset-password', userController.resetPassword);
-
+router.post("/signup", userController.signup);
+router.post("/signin", userController.signin);
+router.post("/forgot-password", userController.forgotPassword);
+router.post("/reset-password", userController.resetPassword);
 
 //all users (only accessible to admin)
-router.delete('/deleteuser/:userId',userController.deleteUser);
+router.delete("/deleteuser/:userId", userController.deleteUser);
 // router.put('/updateuser', authMiddleware, userController.editUser);
-router.put('/updateuser/:userId', userController.updateUser);
-router.get('/allusers', authMiddleware, userController.getAllUsers);
-
+router.put("/updateuser/:userId", userController.updateUser);
+router.get("/allusers", authMiddleware, userController.getAllUsers);
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const userRoutes = require("./Routes/userRoute");
 const verifyRoutes = require("./Routes/verifyRoutes");
-const courseRoutes = require('./Routes/courseRoute');
-const cors = require('cors'); 
+const courseRoutes = require("./Routes/courseRoute");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
-
+app.use(cors());
 mongoose.connect(
   "mongodb+srv://opranjan91700:2vNaWKZeiCwFK5WK@cluster0.7mjn7a6.mongodb.net/",
   { useNewUrlParser: true, useUnifiedTopology: true }
@@ -23,7 +23,7 @@ app.use(cors());
 // Use user routes
 app.use("/user", userRoutes);
 app.use("/verify", verifyRoutes);
-app.use('/user-course', courseRoutes);
+app.use("/user-course", courseRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
